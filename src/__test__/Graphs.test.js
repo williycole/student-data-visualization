f053d3ff92330e4b586fbb4cd72bf0c29f732982
williycole <64283810+williycole@users.noreply.github.com>
2021-09-20 10:01:28 -0500

    Completed one test for project and added demo gif

--- src/__test__/Graphs.test.js
+++ src/__test__/Graphs.test.js
@@ -1,12 +1,12 @@
-// import { expect, test } from "@jest/globals";
-// import { render } from "@testing-library/react";
+import { expect, test } from "@jest/globals";
+import { render } from "@testing-library/react";
 
-// import Graphs from "../components/Graphs";
-// import App from "../components/App";
+import Graphs from "../components/Graphs";
+import App from "../components/App";
 
-// test("displays graphs correctly", async () => {
-//   const graph = render(<Graphs />);
+test("displays graphs correctly", async () => {
+  const graph = render(<Graphs />);
 
-//   const dataGraph = await graph.findByTestId("graph");
-//   expect(dataGraph.find(<Graphs />).exists()).toBeFalsy();
-// });
+  const dataGraph = await graph.findByTestId("graph");
+  expect(dataGraph.find(<Graphs />).exists()).toBeFalsy();
+});
