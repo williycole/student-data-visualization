d3f43188129dd8b2393faf9a4d1c1ca6ae31fd76
williycole <64283810+williycole@users.noreply.github.com>
2021-09-20 08:56:56 -0500

    Backing up at testing work

---
+++ src/__test__/App.test.js
@@ -0,0 +1,20 @@
+import { render, screen } from "@testing-library/react";
+import ShallowRenderer from "react-test-renderer/shallow";
+import Graphs from "../components/Graphs";
+import App from "./App";
+
+test("renders various graphs", async () => {
+  const renderer = new ShallowRenderer();
+  renderer.render(<App />);
+  const res = renderer.getRenderOutput();
+
+  expect(res.type).toBe("div");
+  expect(res.props.children).toEqual([
+    <main
+      className="App-header"
+      className="bg-secondary h-full overflow-hidden "
+    >
+      <Graphs data-testid="graph" />
+    </main>,
+  ]);
+});
